712a38f284a170424baf5b00192a86b68253fc1e
larsyx <gabriele.2000.02@gmail.com>
2023-01-04 20:56:40 +0100

    piccoli miglioramenti, creazioni di nuovi grafici

--- Graphql.py
+++ Graphql.py
@@ -1,27 +1,30 @@
 import requests
 
+
+
 class graphQL:
     def __init__(self, token):
         self.token = token
         self.headers = {"Authorization": f"Bearer {token}"}
+
         super(graphQL, self).__init__()
-    def run_query1(self, query):  # A simple function to use requests.post to make the API call. Note the json= section.
-        request = requests.post('https://api.github.com/graphql', json={'query': query},headers=self.headers)
+
+    def run_query1(self, query):
+        request = requests.post('https://api.github.com/graphql', json={'query': query}, headers=self.headers)
         if request.status_code == 200:
             return request.json()
         else:
             raise Exception("Query failed to run by returning code of {}. {}".format(request.status_code, query))
 
-    def run_query(self, query, variables):  # A simple function to use requests.post to make the API call. Note the json= section.
-        request = requests.post('https://api.github.com/graphql', json={'query': query, 'variables': variables}, headers=self.headers)
+    def run_query(self, query, variables):
+        request = requests.post('https://api.github.com/graphql', json={'query': query, 'variables': variables},
+                                headers=self.headers)
         if request.status_code == 200:
             return request.json()
         else:
             raise Exception("Query failed to run by returning code of {}. {}".format(request.status_code, query))
-
 
     def getClosedIssuesActors(self):
-        listOfNames = []
         query = '''
                 query($owner: String!, $name: String!) { 
 				repository(owner: $owner, name: $name){
@@ -74,18 +77,19 @@ class graphQL:
         resultOrg = self.run_query1(query2)  # execute query
         print("Total number of Organizzazion:", resultOrg["data"]["org"]["userCount"])
 
-    def getTotalRepositoryByLanguage(self,language):
+    def getTotalRepositoryByLanguage(self, language):
         query = '''
             query{
-                search(type: REPOSITORY, query: \"language:'''+ language + '''\") {
+                search(type: REPOSITORY, query: \"language:''' + language + '''\") {
                 repositoryCount
               }
             }'''
 
         result = self.run_query1(query)  # execute query
-        print("Repository in ",language, ":",result["data"]["search"]["repositoryCount"])
+        print("Repository in ", language, ":", result["data"]["search"]["repositoryCount"])
+        return result["data"]["search"]["repositoryCount"]
 
-    def getUserByCountry(self,country):
+    def getUserByCountry(self, country):
         query = '''
           query($country: String!){
               user: search(type: USER, query: $country){
@@ -98,5 +102,5 @@ class graphQL:
         }
 
         result = self.run_query(query, variables)
-        print("Total user in",country + ":",result["data"]["user"]["userCount"])
-
+        print("Total user in", country + ":", result["data"]["user"]["userCount"])
+        result["data"]["user"]["userCount"]
--- main.py
+++ main.py
@@ -5,14 +5,32 @@ import matplotlib.pyplot as plt
 import numpy as np
 import Graphql
 
-access_token = "<your access token>"
+access_token = "<your token>"
 g = Github(access_token)
 larsyx = g.get_user()
+graph = Graphql.graphQL(access_token)
 
-language = ['Java', 'Python', 'C', 'C++', 'Go', 'R',
-            'ZIL', 'JavaScript']
+countries = []
+language = []
 
-countries = ["Germany"]
+
+def creaArrayLanguage():
+    file = open("LanguageList.txt", "r")
+    lines = file.readlines()
+    file.close()
+
+    for line in lines:
+        language.append(line[0: (len(line) - 1)])
+
+
+def creaArrayCountries():
+    file = open("CountriesList.txt", "r")
+    lines = file.readlines()
+    file.close()
+
+    for line in lines:
+        countries.append(line[0: (len(line) - 1)])
+
 
 def trovaThread(repository, author):
     tot_commits = 0
@@ -25,7 +43,7 @@ def trovaThread(repository, author):
     print("#Commits: " + str(tot_commits))
 
 
-def analisiRepository(repo):
+def analysisRepository(repo):
     commits = repo.get_commits()
     contributors = repo.get_contributors()
     languages = repo.get_languages()
@@ -66,9 +84,10 @@ def analisiRepository(repo):
     for r in codeFrequency:
         print("\t" + str(r.week.date()) + " Linee di codice aggiunti: " + str(
             r.additions) + "\t Linee di codice rimossi " + str(r.deletions))
-        weekCode.append(r.week.date())
-        codeAdded.append(r.additions)
-        codeRemoved.append(r.deletions)
+        if r.additions>0 or r.deletions>0 :
+            weekCode.append(r.week.date())
+            codeAdded.append(r.additions)
+            codeRemoved.append(r.deletions)
 
     fig, ax = plt.subplots()
     ax.bar(weekCommit, totalComm)
@@ -97,7 +116,7 @@ def analisiRepository(repo):
 # for coll in contributors:
 #   print("\t" + coll.login + " " + str(coll.contributions))
 
-def analisiUtente(user):
+def analysisUser(user):
     repository = user.get_repos()
     thread = Thread(target=trovaThread, args=(repository, user.login))
     thread.start()
@@ -110,7 +129,7 @@ def analisiUtente(user):
     thread.join()
 
 
-def analisiRepositories():
+def analysisRepositories():
     print("\n\n\nRepository in Python")
     repositories = g.search_repositories(query='language:Python')
     print(repositories.totalCount)
@@ -121,22 +140,26 @@ def analisiRepositories():
         for l in r.get_languages():
             print("\t" + l)
 
+def analysisProgrammingLanguages():
+    resultLanguages = []
+    for lang in language:
+        resultLanguages.append(graph.getTotalRepositoryByLanguage(lang))
+
+    plt.pie(resultLanguages)
+    plt.legend(language,title="Language",bbox_to_anchor=(1,0.5), loc="center right", bbox_transform=plt.gcf().transFigure)
+    plt.show()
 
 def main():
-    # analisiRepository(g.get_repo("apache/kibble-1"))
-    # analisiRepository(g.get_repo("apache/dubbo"))
-    # analisiUtente(g.get_user("taowen"))
-    # analisiRepositories()
-    #analisiUtenti()
+    # analysisRepository(g.get_repo("apache/kibble-1"))
+    analysisRepository(g.get_repo("apache/dubbo"))
+    # analysisUser(g.get_user("taowen"))
+    # analysisRepositories()
+    analysisProgrammingLanguages()
 
-    graph = Graphql.graphQL(access_token)
-    graph.getTotalNumberOfUser()
+
     print("\n\n\n" + str(g.get_rate_limit()))
-    for lang in language:
-        graph.getTotalRepositoryByLanguage(lang)
-
-    for country in countries:
-        graph.getUserByCountry(country)
 
 if __name__ == "__main__":
+    creaArrayLanguage()
+    creaArrayCountries()
     main()
